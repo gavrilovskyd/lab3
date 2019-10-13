179aee08eb3fc0abac153448b11acad82074c328
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-13 20:57:04 +0300

    Scripted auto-commit on change (2019-10-13 20:57:04) by gitwatch.sh

--- src/main/java/ru/labs/flights/DelayStatApp.java
+++ src/main/java/ru/labs/flights/DelayStatApp.java
@@ -12,6 +12,7 @@ public class DelayStatApp {
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         JavaRDD<String> flightLines = sc.textFile("flights_data.csv");
-        JavaPairRDD<Tuple2<String, String>, FlightInfo> airportsDelay = flightLines.mapToPair()
+        JavaPairRDD<Tuple2<String, String>, FlightInfo> airportsDelay =
+                flightLines.mapToPair()
     }
 }
