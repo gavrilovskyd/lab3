791a54fc9db68ed6b84a9a183182214b35b34391
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-13 20:43:16 +0300

    Scripted auto-commit on change (2019-10-13 20:43:16) by gitwatch.sh

--- src/main/java/ru/labs/flights/DelayStatApp.java
+++ src/main/java/ru/labs/flights/DelayStatApp.java
@@ -1,6 +1,7 @@
 package ru.labs.flights;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class DelayStatApp {
@@ -8,6 +9,6 @@ public class DelayStatApp {
         SparkConf conf = new SparkConf().setAppName("DelayStatApp");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-
+        JavaRDD<String> airportLines = sc.textFile("airports_data.csv");
     }
 }
