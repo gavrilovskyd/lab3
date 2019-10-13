423571169e69e9ad6b45ad9f8f4887cb3bc038a3
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-13 21:21:32 +0300

    Scripted auto-commit on change (2019-10-13 21:21:32) by gitwatch.sh

--- src/main/java/ru/labs/flights/DelayStatApp.java
+++ src/main/java/ru/labs/flights/DelayStatApp.java
@@ -38,7 +38,7 @@ public class DelayStatApp {
 
                     return new Tuple4<>(
                             record.get(ORIGIN_AIRPORT_ID_FIELD), record.get(DEST_AIRPORT_ID_FIELD),
-                            record.get(DELAY_FIELD), record.get(CANCELED_FIELD);
+                            record.get(DELAY_FIELD), record.get(CANCELED_FIELD));
                 }).filter(flightInfo -> Float.parseFloat(record.get(CANCELED_FIELD)) < EPS);
 
     }
