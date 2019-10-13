27bb252e2b73759252f765cf02e8b184c33dc702
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-13 21:57:53 +0300

    Scripted auto-commit on change (2019-10-13 21:57:53) by gitwatch.sh

--- src/main/java/ru/labs/flights/BadFlightsInfo.java
+++ src/main/java/ru/labs/flights/BadFlightsInfo.java
@@ -4,7 +4,9 @@ import java.io.Serializable;
 
 public class BadFlightsInfo implements Serializable {
     private float maxDelay;
-    private 
+    private int delayedCount;
+    private int canceledCount;
+    private int totalCount;
 
     private boolean isCanceled;
     private static final double EPS = 1e-6;
