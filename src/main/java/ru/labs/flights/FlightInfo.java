34ea0212bc6ea99614cd510aab9bb41d0b9d9990
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-13 20:51:45 +0300

    Scripted auto-commit on change (2019-10-13 20:51:45) by gitwatch.sh

--- src/main/java/ru/labs/flights/FlightInfo.java
+++ src/main/java/ru/labs/flights/FlightInfo.java
@@ -4,4 +4,11 @@ public class FlightInfo implements {
     private float delay;
     private boolean isCanceled;
 
+    public float getDelay() {
+        return delay;
+    }
+
+    public boolean isCanceled() {
+        return isCanceled;
+    }
 }
