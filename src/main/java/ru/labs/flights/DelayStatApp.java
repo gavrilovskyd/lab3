42f0f049f36c0652640d106c791f0d9fe58a662c
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-13 22:38:58 +0300

    Scripted auto-commit on change (2019-10-13 22:38:58) by gitwatch.sh

--- src/main/java/ru/labs/flights/DelayStatApp.java
+++ src/main/java/ru/labs/flights/DelayStatApp.java
@@ -51,5 +51,6 @@ public class DelayStatApp {
 
                     return new Tuple2<>(record.get(CODE_FIELD), record.get(DESCRIPTION_FIELD));
                 });
+        airportNames.collectAsMap();
     }
 }
