659ade0e0a0a22c845aba196805d32fe8d4c6728
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-13 21:25:13 +0300

    Scripted auto-commit on change (2019-10-13 21:25:13) by gitwatch.sh

--- src/main/java/ru/labs/flights/DelayStatApp.java
+++ src/main/java/ru/labs/flights/DelayStatApp.java
@@ -42,7 +42,7 @@ public class DelayStatApp {
                 })
                 .filter(parsedRecord -> Float.parseFloat(parsedRecord._4())< EPS
                         && !parsedRecord._3().isEmpty() && Float.parseFloat(parsedRecord._3()) > EPS)
-                .mapToPair(parsedRecord -> new Tuple2<>(new Tuple2<>(parsedRecord._1(), parsedRecord._2())));
+                .mapToPair(parsedRecord -> new Tuple2<>(new Tuple2<>(parsedRecord._1(), parsedRecord._2()), Float.parseFloat(parsedRecord._3())));
 
     }
 }
