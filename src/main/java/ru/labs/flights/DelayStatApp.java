cc0c38dbad6844315a11b4153a03d4359030d7f7
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-13 21:07:33 +0300

    Scripted auto-commit on change (2019-10-13 21:07:33) by gitwatch.sh

--- src/main/java/ru/labs/flights/DelayStatApp.java
+++ src/main/java/ru/labs/flights/DelayStatApp.java
@@ -1,5 +1,6 @@
 package ru.labs.flights;
 
+import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
