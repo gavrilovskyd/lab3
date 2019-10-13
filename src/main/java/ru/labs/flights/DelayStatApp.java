36fa3a949bd73cb6440c5a5cba57b40560bc8da7
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-13 20:05:10 +0300

    Scripted auto-commit on change (2019-10-13 20:05:10) by gitwatch.sh

--- src/main/java/ru/labs/flights/DelayStatApp.java
+++ src/main/java/ru/labs/flights/DelayStatApp.java
@@ -1,7 +1,9 @@
 package ru.labs.flights;
 
+import org.apache.spark.SparkConf;
+
 public class DelayStatApp {
     public static void main(String[] args) throws Exception {
-        
+        SparkConf conf = new SparkConf().setAppName("DelayStatApp");
     }
 }
