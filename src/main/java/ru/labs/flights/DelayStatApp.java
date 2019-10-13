7c095a81181d5916b3230eca1d756968c76be031
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-13 21:05:33 +0300

    Scripted auto-commit on change (2019-10-13 21:05:33) by gitwatch.sh

--- src/main/java/ru/labs/flights/DelayStatApp.java
+++ src/main/java/ru/labs/flights/DelayStatApp.java
@@ -18,7 +18,9 @@ public class DelayStatApp {
         JavaPairRDD<Tuple2<String, String>, FlightInfo> airportsDelay =
                 flightLines.mapToPair(line -> {
                     String[] fields = line.split(",");
-                    return new Tuple2<>(new Tuple2<>(fields[ORIGIN_AIRPORT_ID_FILED], fields[DEST_AIRPORT_ID_FILED]))
+                    return new Tuple2<>(
+                            new Tuple2<>(fields[ORIGIN_AIRPORT_ID_FILED], fields[DEST_AIRPORT_ID_FILED]),
+                            new FlightInfo(Float.parseFloat()))
                 });
 
     }
