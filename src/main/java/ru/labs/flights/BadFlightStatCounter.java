cc029de08048ced3c731a9b4bb34b74f269e89e9
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-10-14 23:21:13 +0300

    Scripted auto-commit on change (2019-10-14 23:21:13) by gitwatch.sh

--- src/main/java/ru/labs/flights/BadFlightStatCounter.java
+++ src/main/java/ru/labs/flights/BadFlightStatCounter.java
@@ -2,7 +2,7 @@ package ru.labs.flights;
 
 import java.io.Serializable;
 
-public class BadFlightsStat implements Serializable {
+public class BadFlightStatCounter implements Serializable {
     private static final double EPS = 1e-6;
 
     private float maxDelay;
@@ -10,14 +10,14 @@ public class BadFlightsStat implements Serializable {
     private int canceledCount;
     private int totalCount;
 
-    private BadFlightsStat(float maxDelay, int delayedCount, int canceledCount, int totalCount) {
+    private BadFlightStatCounter(float maxDelay, int delayedCount, int canceledCount, int totalCount) {
         this.maxDelay = maxDelay;
         this.delayedCount = delayedCount;
         this.canceledCount = canceledCount;
         this.totalCount = totalCount;
     }
 
-    public BadFlightsStat(String rawDelay, String rawCanceled) {
+    public BadFlightStatCounter(String rawDelay, String rawCanceled) {
         this.maxDelay = 0.f;
         if (!rawDelay.isEmpty()) {
             this.maxDelay = Float.parseFloat(rawDelay);
@@ -52,8 +52,8 @@ public class BadFlightsStat implements Serializable {
         return ((float) delayedCount / totalCount);
     }
 
-    public static BadFlightsStat add(BadFlightsStat a, BadFlightsStat b) {
-        return new BadFlightsStat(
+    public static BadFlightStatCounter add(BadFlightStatCounter a, BadFlightStatCounter b) {
+        return new BadFlightStatCounter(
                 Math.max(a.getMaxDelay(), b.getMaxDelay()),
                 a.getDelayedCount() + b.getDelayedCount(),
                 a.getCanceledCount() + b.getCanceledCount(),
--- src/main/java/ru/labs/flights/DelayStatApp.java
+++ src/main/java/ru/labs/flights/DelayStatApp.java
@@ -50,17 +50,16 @@ public class DelayStatApp {
         final Broadcast<Map<String, String>> airportNamesBroadcast = sc.broadcast(airportNamesMap);
 
         JavaRDD<String> flightLines = sc.textFile(args[0]);
-        JavaPairRDD<Tuple2<String, String>, BadFlightsStat> airportsBadFlightsStats = flightLines
+        JavaPairRDD<Tuple2<String, String>, BadFlightStatCounter> airportsBadFlightsStats = flightLines
                 .mapToPair(line -> {
                     CSVParser parser = CSVParser.parse(line, CSVFormat.RFC4180.withHeader(flightHeader));
                     CSVRecord record = parser.getRecords().get(0);
 
                     return new Tuple2<>(
                             new Tuple2<>(record.get(ORIGIN_AIRPORT_ID_FIELD), record.get(DEST_AIRPORT_ID_FIELD)),
-                            new BadFlightsStat(record.get(DELAY_FIELD), record.get(CANCELED_FIELD)));
+                            new BadFlightStatCounter(record.get(DELAY_FIELD), record.get(CANCELED_FIELD)));
                 })
-                .reduceByKey(BadFlightsStat::add);
-
+                .reduceByKey(BadFlightStatCounter::add);
 
         //TODO: remove Strings
         JavaRDD<String> totalAirportStat = airportsBadFlightsStats.map(badFlightsStat -> {
